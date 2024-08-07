9ce16da7e39d937baeb781926c1153c92bac70b5
GaneshRodda <roddaganesh3@gmail.com>
2024-08-07 18:25:57 +0530

    adding basic structure of services

---
+++ Microservice1.1/src/test/java/com/nisum/microservice1/Microservice11/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.microservice1.Microservice11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
