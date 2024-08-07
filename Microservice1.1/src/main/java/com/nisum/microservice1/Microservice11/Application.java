e44f9c9d3f39900e27e53fbbd2b13f78dd7bf024
GaneshRodda <roddaganesh3@gmail.com>
2024-08-07 20:05:16 +0530

    changes in code

--- Microservice1.1/src/main/java/com/nisum/microservice1/Microservice11/Application.java
+++ Microservice1.1/src/main/java/com/nisum/microservice1/Microservice11/Application.java
@@ -1,13 +1,24 @@
 package com.nisum.microservice1.Microservice11;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
+	@Value("${server.port}")
+	private String port;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/micro1")
+	public String test(){
+		return port;
 	}
 
 }
